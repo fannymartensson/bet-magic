7871c24ec9e125b0b260b7c390cc6f794b53b0d6
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-02-09 21:40:27 +0100

    created nav page

---
+++ src/components/LogoIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <button>
+    <img
+      v-if="logoIcon"
+      class="logo-img"
+      src="../assets/logo-text.svg"
+      alt="bet-magic-logo"
+    />
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'LogoIcon',
+    props: {
+      logoIcon: {
+        type: Boolean
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  button {
+    background: transparent;
+    border: none;
+    margin: 2rem;
+    cursor: pointer;
+  }
+  .logo-img {
+    max-width: 20rem;
+  }
+</style>
---
+++ src/views/NavigationBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="background-img">
+    <logo-icon class="logo-btn" :logoIcon="true" />
+    <nav>
+      <ul class="menu">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">LOGIN</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">OUR GAMES</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">FAQ</router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script>
+  import LogoIcon from '@/components/LogoIcon.vue'
+
+  export default {
+    name: 'NavigationBar',
+    components: {
+      LogoIcon
+    }
+  }
+</script>
+
+<style>
+  .background-img {
+    background-image: url('../assets/Backgrund.svg');
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-attachment: fixed;
+  }
+  .logo-icon {
+    display: flex;
+    justify-content: center;
+  }
+  .menu {
+    padding: 2rem;
+    text-align: center;
+  }
+  .nav-link {
+    text-decoration: none;
+    color: #fff;
+    font-size: 30px;
+  }
+  .nav-item {
+    padding: 1rem;
+  }
+</style>
