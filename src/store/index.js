abb995d418c87ee47b8d2a90e612dbf44b154056
samusun <samuelsinkorg@gmail.com>
2021-02-10 13:55:05 +0100

    prettier krånglar

--- src/store/index.js
+++ src/store/index.js
@@ -4,45 +4,41 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    currentIndex: 0,
-    cards: [
-      {
-        title: 'Game 1',
-        description:
-          'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
-        img: '/gameCard.jpg'
-      },
-      {
-        title: 'Game 2',
-        description:
-          'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
-        img: '/gameCard.jpg'
-      },
-      {
-        title: 'Game 3',
-        description:
-          'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
-        img: '/gameCard.jpg'
-      }
-    ]
-  },
-  getters: {
-    currentIndex: state => {
-      return state.currentIndex
+    state: {
+        currentIndex: 0,
+        cards: [{
+                title: 'Game 1',
+                description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                img: '/gameCard.jpg'
+            },
+            {
+                title: 'Game 2',
+                description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                img: '/gameCard.jpg'
+            },
+            {
+                title: 'Game 3',
+                description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                img: '/gameCard.jpg'
+            }
+        ]
     },
-    cards: state => {
-      return state.cards
-    }
-  },
-  mutations: {
-    nextIndex: state => {
-      if (state.currentIndex < state.cards.length - 1) state.currentIndex++
+    getters: {
+        currentIndex: state => {
+            return state.currentIndex
+        },
+        cards: state => {
+            return state.cards
+        }
     },
-    prevIndex: state => {
-      if (state.currentIndex > 0) state.currentIndex--
-    }
-  },
-  actions: {},
-  modules: {}
+    mutations: {
+        nextIndex: state => {
+            if (state.currentIndex < state.cards.length - 1) state.currentIndex++
+        },
+        prevIndex: state => {
+            if (state.currentIndex > 0) state.currentIndex--
+        }
+    },
+    actions: {},
+    modules: {}
 })
