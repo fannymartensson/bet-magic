3be76e02624a460027ebe33e6091e25432444289
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-02-10 18:29:58 +0100

    fixed button

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,45 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
      state: {
-        login: true
+        login: true,
+        currentIndex: 0,
+        cards: [
+          {
+            title: 'Space Invadors',
+            description:
+              'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+            img: '/space-invadors.png'
+          },
+          {
+            title: 'Pac Man',
+            description:
+              'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+            img: '/pacman.png'
+          },
+          {
+            title: 'Donky Kong',
+            description:
+              'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+            img: '/donkykong.png'
+          }
+        ]
     },
-    mutations: {},
+    getters: {
+        currentIndex: state => {
+            return state.currentIndex
+          },
+          cards: state => {
+            return state.cards
+          }
+    },
+    mutations: {
+        nextIndex: state => {
+            if (state.currentIndex < state.cards.length - 1) state.currentIndex++
+          },
+          prevIndex: state => {
+            if (state.currentIndex > 0) state.currentIndex--
+          }
+    },
     actions: {},
     modules: {}
 })
