686cbe8abf130113e62df3225225df9fba0c6f87
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-02-10 12:40:22 +0100

    login design fixed

---
+++ src/components/MenuBtn.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="menu-btn" v-if="menuBtn">MENU</button>
+</template>
+<script>
+  export default {
+    name: 'MenuBtn',
+    props: {
+      menuBtn: {
+        type: Boolean
+      }
+    }
+  }
+</script>
+
+<style>
+  header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+  .logo-img {
+    width: 139px;
+    margin-left: -2rem;
+  }
+  .menu-btn {
+    background: transparent;
+    color: #fff;
+    border: 1px solid white;
+    padding: 9px 11px;
+    font-family: 'Arcade', Avenir, Helvetica, Arial, sans-serif;
+    font-size: 2rem;
+    cursor: pointer;
+    height: 4rem;
+    margin-top: 6rem;
+    border-radius: 6px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import About from '@/views/About.vue'
 import NavigationBar from '@/views/NavigationBar.vue'
+import LoginPage from '@/views/LoginPage.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage
   }
 ]
 
