6d4988ac8418b9cdfc843c90d21fb8ef8910f250
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-02-11 16:02:10 +0100

    route nav

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import OurGames from '@/views/OurGames.vue'
 import NavigationBar from '@/views/NavigationBar.vue'
 import SignUp from '@/components/SignUp.vue'
 import LoginPage from '@/views/LoginPage.vue'
+import ProfilePage from '@/components/ProfilePage.vue'
 import TicTac from '@/views/TicTac.vue'
 
 Vue.use(VueRouter)
@@ -33,11 +34,29 @@ const routes = [
     path: '/signup',
     name: 'SignUp',
     component: SignUp
+  },
+  {
+    path: '/profile',
+    name: 'ProfilePage',
+    component: ProfilePage,
+    meta: {
+      requiresAuth: true
+    }
   }
 ]
-
 const router = new VueRouter({
   routes
+})
+//NAVIGATION GUARD
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(routes => routes.meta.requiresAuth)) {
+    if (localStorage.getItem('auth') === 'magic@yahoo.se') {
+      next()
+    } else {
+      next({ path: '*' })
+    }
+  }
+  next()
 })
 
 export default router
