eedd1f2f4701f10f98e9c80b5a05f94895253aa9
samusun <samuelsinkorg@gmail.com>
2021-02-11 18:50:49 +0100

    nån error bara

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -9,25 +9,46 @@
       <form class="signup-form">
         <h1 class="signup-title">Sign Up</h1>
         <div class="textgroup">
-          <input id="firstname" type="text" placeholder="First Name" v-model="firstname"
-            required/>
+          <input
+            id="firstname"
+            type="text"
+            placeholder="First Name"
+            v-model="firstname"
+            required
+          />
         </div>
         <div class="textgroup">
-          <input id="lastname" type="text" placeholder="Last Name" v-model="lastname"
-            required/>
+          <input
+            id="lastname"
+            type="text"
+            placeholder="Last Name"
+            v-model="lastname"
+            required
+          />
         </div>
         <div class="textgroup">
-          <input id="emailid" type="text" placeholder="Email Id" v-model="emailid"
-            required />
+          <input
+            id="emailid"
+            type="text"
+            placeholder="Email Id"
+            v-model="emailid"
+            required
+          />
         </div>
         <div class="textgroup">
-          <input id="password" type="text" placeholder="Password" v-model="password"
-            required/>
+          <input
+            id="password"
+            type="text"
+            placeholder="Password"
+            v-model="password"
+            required
+          />
         </div>
-         <input class="form-btn" @click="first" type="button" value="Sign Up" />       
-         <div > Already have an account?<router-link to="*">Login here?</router-link>
-         </div>
-        </form>
+        <input class="form-btn" @click="first" type="button" value="Sign Up" />
+        <div>
+          Already have an account?<router-link to="*">Login here?</router-link>
+        </div>
+      </form>
     </main>
   </div>
 </template>
@@ -35,28 +56,25 @@
   import MenuBtn from '@/components/MenuBtn.vue'
   import LogoIcon from '@/components/LogoIcon.vue'
 
-export default {
-    name: "Signup",
+  export default {
+    name: 'Signup',
     components: { MenuBtn, LogoIcon },
-   
-  data: function() {
-    return {
-      firstname: '',
-      lastname: '',
-      emailid: '',  
-      password: '',
-      confirmpassword: ''
-    }
+
+    data: function() {
+      return {
+        firstname: '',
+        lastname: '',
+        emailid: '',
+        password: '',
+        confirmpassword: ''
+      }
     },
-    methods:{
-    first() {       
-      alert(
-        'Thank you for registering with BetMagic'
-      )
+    methods: {
+      first() {
+        alert('Thank you for registering with BetMagic')
+      }
     }
   }
-}
-  
 </script>
 <style scoped>
   main {
