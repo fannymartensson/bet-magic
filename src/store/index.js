7dfd7f29fa331a18508456b959656e20aeb02c9a
shrujanapeesari <70943400+shrujanapeesari@users.noreply.github.com>
2021-02-16 11:52:29 +0100

    modified index

--- src/store/index.js
+++ src/store/index.js
@@ -82,7 +82,7 @@ export default new Vuex.Store({
           'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
         img: '/snakegame.png',
         routerlink: 'SnakeGame'
-      },
+      }
     ]
     /* HÄR SLUTAR GAMING CARDSEN */
   },
