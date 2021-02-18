7d44a11f2d721d7be0d79fe4bafffea71f47880b
samusun <samuelsinkorg@gmail.com>
2021-02-18 08:53:21 +0100

    removed return in mastermind

--- src/components/mastermind/Mastermind.vue
+++ src/components/mastermind/Mastermind.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="bigPic">
     <main id="Mastermind">
-      <Return id="Return" />
       <Restart />
       <h1>Mastermind</h1>
       <Board />
@@ -23,7 +22,6 @@
 <script>
   import { mapGetters, mapState, mapActions } from 'vuex'
 
-  import Return from '../return.vue'
   import Board from './Board.vue'
   import Feedback from './Feedback.vue'
   import Check from './Check.vue'
@@ -37,7 +35,6 @@
       Feedback,
       Check,
       Dialog,
-      Return,
       Restart
     },
     computed: {
