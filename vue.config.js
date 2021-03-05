5d4ba1e7c2472ff6292569d766fa5723ec75c147
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-03-05 17:52:07 +0100

    fixed service workers

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,25 @@
 module.exports = {
+  publicPath: '/',
   pwa: {
-    runtimeCompiler: true,
-    publicPath: '/',
-    workboxPluginMode: 'InjectManifest',
     manifestOptions: {
-      background_color: '#261b40'
+      workboxPluginMode: 'InjectManifest',
+      background_color: '#261b40',
+      display: 'standalone',
+      name: 'bat-magic',
+      short_name: 'BM',
+      start_url: '.',
+      themeColor: '#261b40'
     },
-    name: 'Bet Magic',
-    themeColor: '#261b40',
     workboxOptions: {
       runtimeCaching: [
         {
           handler: 'NetworkFirst',
           options: {
-            networkTimeoutSeconds: 5
-          }
-          // urlPattern:
+            networkTimeoutSeconds: 11
+          },
+          urlPattern: 'data.json'
         }
       ]
     }
   }
 }
-
-// add what you prefer
